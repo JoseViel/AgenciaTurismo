fe9d26d7427f6b2c9e4817c8d46f97f983de3492
JoseViel <jose.viel@outlook.com>
2023-11-07 11:38:41 -0300

    logout

--- choose.php
+++ choose.php
@@ -1,3 +1,16 @@
+<?php
+
+session_start();
+    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
+    {
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header("location: login.php");
+    } 
+
+$id = $_SESSION['id'];
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -48,20 +61,6 @@
     </style>
 </head>
 <body>
-        <div class="logo_header">
-            <a> </a>
-        </div>
-        <div class="navigation_header" id="navigation_header">
-            <button onclick="toggleSidebar()" class="btn_icon_header">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                </svg>
-            </button>
-            <a href="#" class="active">Home</a>
-            <a href="#">Cursos</a>
-            <a href="#">Sobre nós</a>
-            <a href="#">Contato</a>
-        </div>
     <section class="area-login">
         <div class="choose">
             <p style="font-weight: bold;
@@ -96,47 +95,5 @@
         </div>
     </section>
 </body>
-
-<script>
-    var header           = document.getElementById('header');
-    var navigationHeader = document.getElementById('navigation_header');
-    var content          = document.getElementById('content');
-    var showSidebar      = false;
-
-    function toggleSidebar()
-    {
-        showSidebar = !showSidebar;
-        if(showSidebar)
-        {
-            navigationHeader.style.marginLeft = '-10vw';
-            navigationHeader.style.animationName = 'showSidebar';
-            content.style.filter = 'blur(2px)';
-        }
-        else
-        {
-            navigationHeader.style.marginLeft = '-100vw';
-            navigationHeader.style.animationName = '';
-            content.style.filter = '';
-        }
-    }
-
-    function closeSidebar()
-    {
-        if(showSidebar)
-        {
-            showSidebar = true;
-            toggleSidebar();
-        }
-    }
-
-    window.addEventListener('resize', function(event) {
-        if(window.innerWidth > 768 && showSidebar) 
-        {  
-            showSidebar = true;
-            toggleSidebar();
-        }
-    });
-
-</script>
 
 </html>
---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+
+    session_start();
+    
+    unset($_SESSION['email']);
+    unset($_SESSION['senha']); 
+    unset($_SESSION['id']);
+
+    header("location:login.php"); 
+?>
