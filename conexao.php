18c2c7882c9da263c81050b3e6a36610dffe7d17
JoseViel <jose.viel@outlook.com>
2023-11-07 11:32:18 -0300

    conexao

---
+++ conexao.php
@@ -0,0 +1,31 @@
+<?php
+
+    class Conexao{
+        private static $dbNome = 'agencia';
+        private static $dbHost = 'localhost';
+        private static $dbUsuario = 'root';
+        private static $dbSenha = '';
+
+        private static $cont = null;
+
+        public function __construct()
+        {
+            die("A função init não é permitida");
+        }
+
+        public static function conectar(){
+            if (self::$cont == null){
+                try {
+                    self::$cont = new PDO('mysql:host=localhost; dbname=agencia', self::$dbUsuario, self::$dbSenha);
+                }
+                catch(PDOException $exception){
+                    die ($exception->getmessage());
+                }
+            }
+            return self::$cont;
+        }
+
+        public static function desconectar(){
+            self::$cont = null;
+        } 
+    }
